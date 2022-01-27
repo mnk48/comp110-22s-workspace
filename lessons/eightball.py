7bf1c2d90594fc6a6e891c72c69b54ccf9a36c21
Mia Keller <mnk48@email.unc.edu>
2022-01-27 10:44:55 -0500

    finished again

--- lessons/eightball.py
+++ lessons/eightball.py
@@ -8,11 +8,9 @@ response: int = randint(0, 3)
 
 if response == 0:
     print("Most definitely")
+elif response == 1:
+    print("Highly likely")
+elif response == 2:
+    print("Ask again later")
 else:
-    if response == 1:
-        print("Highly likely")
-    else: 
-        if response == 2:
-            print("Ask again later")
-        else:
-            print("No way, not a chance")
+    print("No way, not a chance")
