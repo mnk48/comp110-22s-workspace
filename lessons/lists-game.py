cad440f547f6ee297395661a5715094b1a2cce8a
Mia Keller <mnk48@email.unc.edu>
2022-02-08 13:51:17 -0500

    done

--- lessons/lists-game.py
+++ lessons/lists-game.py
@@ -4,19 +4,38 @@
 from random import randint
 
 rolls: list[int] = list()  # Setting up an empty list (constructor)
-rolls.append(randint(1, 6))  # Method call 
-rolls.append(randint(1, 6))
-rolls.append(randint(1, 6))
+
+while len(rolls) == 0 or rolls[len(rolls) - 1] != 1:
+    rolls.append(randint(1, 6))
+
 print(rolls)
 
-# Access an individual item 
-print(rolls[0])
-print(rolls[1])
+#  Remove an item from the list by its index ("pop")
+rolls.pop(len(rolls) - 1)
+print(rolls)
 
-#  Access the length of a list (mumber of items)
-print(len(rolls))
+#  Sum the values of our rolls!
+i: int = 0
+sum: int = 0
+while i < len(rolls):
+    sum = sum + rolls[i]
+    i = i + 1
 
-#  Access the last item of a list
-last_index: int = len(rolls) - 1
-print(rolls[last_index])
+print(f"Total score: {sum}")
 
+
+# rolls.append(randint(1, 6))  # Method call 
+# rolls.append(randint(1, 6))
+# rolls.append(randint(1, 6))
+# print(rolls)
+
+# # Access an individual item 
+# print(rolls[0])
+# print(rolls[1])
+
+# #  Access the length of a list (mumber of items)
+# print(len(rolls))
+
+# #  Access the last item of a list
+# last_index: int = len(rolls) - 1
+# print(rolls[last_index])
