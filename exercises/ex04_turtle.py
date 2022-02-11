9e70347cc2ef3a0dd7d2a854844381ab3564eabc
Mia Keller <mnk48@email.unc.edu>
2022-02-11 16:27:53 -0500

    saving 2

--- exercises/ex04_turtle.py
+++ exercises/ex04_turtle.py
@@ -2,27 +2,40 @@
 
 __author__ = "730389267"
 
+from random import randint
 from turtle import Turtle, colormode, done
 colormode(255)
-from random import randint
 
 
 def main() -> None:
     """The entrypoint of my scene."""
+    random_stars()
+    done()
+
 
 def draw_star() -> None:
-    
+    star: Turtle = Turtle()
+    i: int = 0
+    while i < 5:
+        star.forward(20)
+        star.right(144)
+        i += 1
 
 def random_stars() -> None:
     """Randomizes stars in the night sky."""
     star: Turtle = Turtle()
+    star.hideturtle()
     i: int = 0
-    while (i < 10):
+    while (i < 3):
+        x: float = float(randint(-300, 300))
+        y: float = float(randint(-300, 300))
         star.penup()
-        star.goto(randint(-200, 200), randint(-200, 200))
+        star.goto(x, y)
+        star.setheading(0.0)
         star.pendown()
-
-
+        draw_star()
+        i += 1
+    done()
 
 
 def night_sky() -> None:
