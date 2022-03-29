b8d744dfd98645f98f479ec3c1a79cf4f89c4ec6
Mia Keller <mnk48@email.unc.edu>
2022-03-29 10:39:31 -0400

    finished

--- lessons/point.py
+++ lessons/point.py
@@ -16,12 +16,32 @@ class Point:
         self.x *= factor 
         self.y *= factor 
     
+    def __mul__(self, factor: float) -> Point:
+        """Overload the multiplication operator for Point * float."""
+        print("__mul__ was called")
+        return Point(self.x * factor, self.y * factor)
+
+    def __add__(self, rhs: Point) -> Point:
+        print("__add__ was called")
+        return Point(self.x + rhs.x, self.y + rhs.y)
+
+    def __getitem__(self, index: int) -> float:
+        """Overload the subscription notation."""
+        if index == 0:
+            return self.x
+        elif index == 1:
+            return self.y
+        else:
+            raise IndexError
+
     def scale(self, factor: float) -> Point:
-        """Immutable: multiplies components by factor without mutation."""
-        x: float = self.x * factor
-        y: float = self.y * factor 
-        scaled_point: Point = Point(x, y)
-        return scaled_point 
+        """Scale a Point's attributes by factor."""
+        # """Immutable: multiplies components by factor without mutation."""
+        # x: float = self.x * factor
+        # y: float = self.y * factor 
+        # scaled_point: Point = Point(x, y)
+        # return scaled_point 
+        return Point(self.x * factor, self.y * factor)
 
     def __str__(self) -> str:
         """Produce a str representation of a Point for humans."""
@@ -33,11 +53,21 @@ class Point:
         return f"Point({self.x}, {self.y})"
 
 
-p0: Point = Point(1.0, 2.0)
-p1: Point = p0.scale(2.0)
-print(p0)
-p1_as_a_str: str = str(p1)
-print(p1_as_a_str)
+# p0: Point = Point(1.0, 2.0)
+# p1: Point = p0.scale(2.0)
+# print(p0)
+# p1_as_a_str: str = str(p1)
+# print(p1_as_a_str)
 
-p1_repr: str = repr(p1)
-print(p1_repr)
+# p1_repr: str = repr(p1)
+# print(p1_repr)
+
+a: Point = Point(1.0, 2.0)
+b: Point = a * 2.0
+c: Point = a + b
+print(f"a: {a}")
+print(f"b: {b}")
+print(f"c: {c}")
+
+print(a[0])
+print(a[1])
