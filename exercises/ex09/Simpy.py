4e5d6b182c8b66538d5bea4bc7b0c8300dc5a7d5
Mia Keller <mnk48@email.unc.edu>
2022-04-01 16:22:38 -0400

    checkpoint #1

--- exercises/ex09/Simpy.py
+++ exercises/ex09/Simpy.py
@@ -4,10 +4,78 @@ from __future__ import annotations
 
 from typing import Union
 
-__author__ = "YOUR PID HERE"
+__author__ = "730389267"
 
 
 class Simpy:
     values: list[float]
 
-    # TODO: Your constructor and methods will go here.
+    def __init__(self, values):
+        """Initializes Simpy's values to the inputed values list."""
+        self.values = values
+    
+    def __str__(self) -> str:
+        """Returns a string representation of a Simpy object."""
+        return f"Simpy({str(self.values)}"
+    
+    def fill(self, input: float, number: int) -> None:
+        """Given a inputed float and length, fills a Simpy's values with that float and given length."""
+        self.values = []
+        i: int = 0
+        while i < number:
+            self.values.append(input)
+            i += 1
+    
+    def arange(self, start: float, stop: float, step: float = 1.0) -> None:
+        """Given a start, stop, and step float, mutates a Simpy object to produce a list of floats starting with the start index,
+        ending with the number before the stop index, and increasing or decreasing by the step index.
+        """
+        self.values = []
+        i: int = 0
+        while (start + (i * step)) != stop:
+            self.values.append(start + (i * step))
+            i += 1
+    
+    def sum(self) -> float:
+        """Calculates the sum of the values within a Simpy object list."""
+        result: float = sum(self.values)
+        return result
+    
+    def __add__(self, rhs: Union[float, Simpy]) -> Simpy:
+        """Given a float or a Simpy object, adds the float values to each self index
+        or the corresponding Simpy indices together to return a new Simpy object.
+        """
+        result: Simpy = Simpy([])
+        if isinstance(rhs, Simpy):
+            assert len(rhs.values) == len(self.values)
+            i: int = 0
+            while i < len(rhs.values):
+                result.values.append(rhs.values[i] + self.values[i])
+                i += 1
+        else:
+            i: int = 0
+            while i < len(self.values):
+                result.values.append(self.values[i] + rhs)
+                i += 1 
+        return result
+    
+    def __pow__(self, rhs: Union[float, Simpy]) -> Simpy:
+        """Given a float or a Simpy, raises the values of the Simpy object to that
+        float or corresponding Simpy index.
+        """
+        result: Simpy = Simpy([])
+        if isinstance(rhs, Simpy):
+            assert len(rhs.values) == len(self.values)
+            i: int = 0 
+            while i < len(rhs.values):
+                result.values.append(self.values[i] ** rhs.values[i])
+                i += 1
+        else:
+            i: int = 0
+            while i < len(self.values):
+                result.values.append(self.values[i] ** rhs)
+                i += 1
+        return result
+
+    def __eq__(self, )
+    
